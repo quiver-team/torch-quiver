a79f8cef654158c574aa4b4b1ea58e1faa9d8ef6
lg <lg4869@outlook.com>
2020-09-17 14:02:23 +0000

    update

--- srcs/cpp/include/quiver/sparse.hpp
+++ srcs/cpp/include/quiver/sparse.hpp
@@ -1,25 +1,33 @@
 #pragma once
-#include <algorithm>
 #include <stdexcept>
 #include <vector>
 
 namespace quiver
 {
 template <typename T>
-std::vector<T> compress_row_idx(const T n, const std::vector<T> &sorted_row_idx)
+bool compress_row_idx(const std::vector<T> &row_idx, std::vector<T> &row_ptr)
 {
-    const T m = sorted_row_idx.size();
-    std::vector<T> r(n);
+    const T n = row_ptr.size();
+    const T m = row_idx.size();
     T offset = 0;
     for (T i = 0; i < n; ++i) {
         T j = offset;
         for (; j < m; ++j) {
-            if (sorted_row_idx[j] != i) { break; }
+            if (row_idx[j] != i) { break; }
         }
-        r[i] = offset;
+        row_ptr[i] = offset;
         offset = j;
     }
-    if (offset != m) { throw std::invalid_argument("row_idx is not sorted"); }
-    return r;
+    return offset == m;
+}
+
+template <typename T>
+std::vector<T> compress_row_idx(const T n, const std::vector<T> &row_idx)
+{
+    std::vector<T> row_ptr(n);
+    if (!compress_row_idx(row_idx, row_ptr)) {
+        throw std::invalid_argument("row_idx is not sorted");
+    }
+    return row_ptr;
 }
 }  // namespace quiver

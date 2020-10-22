11edd0626b432cca273a262b6997999551810696
ZenoTan <zenotan1998@gmail.com>
2020-10-22 22:06:40 +0800

    Format2

--- srcs/cpp/src/quiver/quiver.cpp
+++ srcs/cpp/src/quiver/quiver.cpp
@@ -1,18 +1,19 @@
 #include <quiver/quiver.cpu.hpp>
 #include <quiver/quiver.hpp>
 
-namespace quiver {
+namespace quiver
+{
 Quiver::Quiver() {}
 
 std::unique_ptr<Quiver>
-Quiver::from_edge_index_cpu(int n,
-                            std::vector<std::pair<int, int>> edge_index) {
-  using T = int64_t;
-  using Q = quiver<T, CPU>;
-  using vec = std::vector<std::pair<T, T>>;
-  vec ei(edge_index.size());
-  std::copy(edge_index.begin(), edge_index.end(), ei.begin());
-  edge_index.clear();
-  return std::make_unique<Q>(n, std::move(ei));
+Quiver::from_edge_index_cpu(int n, std::vector<std::pair<int, int>> edge_index)
+{
+    using T = int64_t;
+    using Q = quiver<T, CPU>;
+    using vec = std::vector<std::pair<T, T>>;
+    vec ei(edge_index.size());
+    std::copy(edge_index.begin(), edge_index.end(), ei.begin());
+    edge_index.clear();
+    return std::make_unique<Q>(n, std::move(ei));
 }
-} // namespace quiver
+}  // namespace quiver

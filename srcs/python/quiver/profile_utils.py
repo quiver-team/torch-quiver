2961df1165fac3fd67297b2681af1b15038f031d
ZenoTan <41138939+ZenoTan@users.noreply.github.com>
2020-10-30 20:13:04 +0800

    Improve prof util (#15)

--- srcs/python/quiver/profile_utils.py
+++ srcs/python/quiver/profile_utils.py
@@ -1,11 +1,28 @@
 import time
 
 
+class Record(object):
+    def __init__(self, name=''):
+        self._last = time.time()
+        self._acc = 0.0
+        self._name = name if name else 'total'
+
+    def on(self):
+        self._last = time.time()
+
+    def off(self):
+        self._acc += time.time() - self._last
+
+    def show(self):
+        print('%s took %.3fs' % (self._name, self._acc))
+
+
 class StopWatch(object):
     def __init__(self, name=''):
         self._name = name if name else 'total'
         self._t0 = time.time()
         self._last = self._t0
+        self._rec = {}
 
     def tick(self, name):
         t = time.time()
@@ -13,7 +30,17 @@ class StopWatch(object):
         self._last = t
         print('%s took %.3fs' % (name, d))
 
+    def turn_on(self, name):
+        if name not in self._rec:
+            self._rec[name] = Record(name)
+        self._rec[name].on()
+
+    def turn_off(self, name):
+        self._rec[name].off()
+
     def __del__(self):
+        for rec in self._rec:
+            self._rec[rec].show()
         t = time.time()
         d = t - self._t0
         print('%s took %.3fs' % (self._name, d))

23ef2fc5731c82304df10dd6aec818f6e1b00205
lg <lg4869@outlook.com>
2020-11-10 13:01:40 +0000

    fix

--- srcs/cpp/include/quiver/algorithm.cu.hpp
+++ srcs/cpp/include/quiver/algorithm.cu.hpp
@@ -41,11 +41,11 @@ void async_transform(const cudaStream_t stream, InputIt first1, InputIt last1,
 }
 
 template <class InputIt, class UnaryOperation>
-__host__ void for_each_kern(int size, InputIt first1, UnaryOperation f)
+__host__ void for_each_kern(int size, InputIt first, UnaryOperation f)
 {
     const int worker_idx = blockIdx.x * blockDim.x + threadIdx.x;
     const int worker_count = gridDim.x * blockDim.x;
-    for (int i = worker_idx; i < size; i += worker_count) { f(*(first1 + i)); }
+    for (int i = worker_idx; i < size; i += worker_count) { f(*(first + i)); }
 }
 
 template <class InputIt, class UnaryFunction>
@@ -53,8 +53,8 @@ void asyc_for_each(const cudaStream_t stream, InputIt first, InputIt last,
                    UnaryFunction f)
 {
     const auto o = get_kernal_option();
-    const int size = thrust::distance(first1, last1);
+    const int size = thrust::distance(first, last);
     for_each_kern<<<o.blocks_per_grid, o.threads_per_block, o.shm_size,
-                    stream>>>(size, first1, f);
+                    stream>>>(size, first, f);
 }
 }  // namespace quiver

c07316e3c87f57a363f44cdf0b9a88961d8cae62
eedalong <ee_dalong@163.com>
2021-08-24 11:32:41 +0800

    fix

--- benchmarks/ogbn_products_sage/proc_zero_copy.py
+++ benchmarks/ogbn_products_sage/proc_zero_copy.py
@@ -330,7 +330,7 @@ if __name__ == '__main__':
     sample_comm = CommConfig(0, 0, 1, [], 1)
     train_comm = CommConfig(1, 0, 1, [], [])
     sync = SyncManager(2, 32)
-    sample = SamplerProcess(sample_comm, sync)
+    sample = CudaSamplerProcess(sample_comm, sync)
     train = TrainerProcess(train_comm, sync)
     root = "/home/dalong/data"
     dataset = PygNodePropPredDataset('ogbn-products', root)

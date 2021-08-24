88bee8f3a7632eb43c1abf147ba9a565184f22b6
eedalong <ee_dalong@163.com>
2021-08-24 12:03:22 +0800

    add

--- benchmarks/ogbn_products_sage/proc_zero_copy.py
+++ benchmarks/ogbn_products_sage/proc_zero_copy.py
@@ -453,7 +453,7 @@ def get_csr_from_coo(edge_index):
     dst = edge_index[1].numpy()
     node_count = max(np.max(src), np.max(dst))
     data = np.zeros(dst.shape, dtype=np.int32)
-    csr_mat = csr_matrix((data, (edge_index[0].numpy(), edge_index[1].numpy())), shape=(node_count, node_count))
+    csr_mat = csr_matrix((data, (edge_index[0].numpy(), edge_index[1].numpy())))
     return csr_mat
 
 if __name__ == '__main__':

4c973eef3d3aa96ff72f4810fe37a340c1aacd46
eedalong <ee_dalong@163.com>
2021-08-29 18:37:19 +0800

    add log

--- tests/python/cuda/test_shard_tensor.py
+++ tests/python/cuda/test_shard_tensor.py
@@ -4,11 +4,11 @@ import torch_quiver as qv
 def test_shard_tensor_intra_process():
     device_0_tensor = torch.randint(0, 100, (100, 30), device = "cuda:0", dtype = torch.float32)
     device_1_tensor = torch.randint(0, 100, (50, 30), device = "cuda:1", dtype=torch.float32)
+    print(f"device_0_tensor device {device_0_tensor.device}\ndevice_1_tensor device {device_1_tensor.device}")
     shard_tensor = qv.ShardTensor(0)
     shard_tensor.add(device_0_tensor, 0)
     shard_tensor.add(device_1_tensor, 100)
-    print(shard_tensor.shape())
-    print(shard_tensor.numel())
+    print("shard_tensor shape = ", shard_tensor.shape())
 
 test_shard_tensor_intra_process()
     

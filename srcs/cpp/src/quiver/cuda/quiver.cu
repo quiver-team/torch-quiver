2ec5bf356366d33fc675265db9dd692c300fec66
eedalong <ee_dalong@163.com>
2021-08-29 19:50:40 +0800

    fix get item

--- srcs/cpp/src/quiver/cuda/quiver.cu
+++ srcs/cpp/src/quiver/cuda/quiver.cu
@@ -992,7 +992,7 @@ void register_cuda_quiver(pybind11::module &m)
     py::class_<quiver::ShardTensor>(m, "ShardTensor")
         //.def(py::init<std::vector<torch::Tensor>, py::array_t<int64_t> ,int>()),
         .def(py::init<int>())
-        .def("__get_item__", &quiver::ShardTensor::operator[], py::call_guard<py::gil_scoped_release>())
+        .def("__getitem__", &quiver::ShardTensor::operator[], py::call_guard<py::gil_scoped_release>())
         .def("shape", &quiver::ShardTensor::shape, py::call_guard<py::gil_scoped_release>())
         .def("numel", &quiver::ShardTensor::numel, py::call_guard<py::gil_scoped_release>())
         .def("device", &quiver::ShardTensor::device, py::call_guard<py::gil_scoped_release>())

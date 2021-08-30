27e836e361fbf379d9e5ea8ef1fcbb3cb3f78f52
ZenoTan <zenotan1998@gmail.com>
2021-08-30 10:42:34 +0000

    update

--- tests/python/cuda/test_shard_tensor.py
+++ tests/python/cuda/test_shard_tensor.py
@@ -2,8 +2,10 @@ import torch
 import torch_quiver as qv
 import random
 import time
-import numpy as np 
+import numpy as np
 import sys
+
+
 def test_shard_tensor_intra_process():
     NUM_ELEMENT = 1000000
     SAMPLE_SIZE = 80000
@@ -11,35 +13,40 @@ def test_shard_tensor_intra_process():
     #########################
     # Init With Numpy
     ########################
-    host_tensor = np.random.randint(0, high=10, size=(2 * NUM_ELEMENT, FEATURE_DIM))
+    host_tensor = np.random.randint(
+        0, high=10, size=(2 * NUM_ELEMENT, FEATURE_DIM))
     host_indice = np.random.randint(0, 2 * NUM_ELEMENT - 1, (SAMPLE_SIZE, ))
-    
-    device_0_tensor = torch.from_numpy(host_tensor[: NUM_ELEMENT]).type(torch.float32).to("cuda:0")
-    device_1_tensor =  torch.from_numpy(host_tensor[NUM_ELEMENT:]).type(torch.float32).to("cuda:1")
-    
-    print(f"device_0_tensor device {device_0_tensor.device}\ndevice_1_tensor device {device_1_tensor.device}")
+
+    device_0_tensor = torch.from_numpy(
+        host_tensor[: NUM_ELEMENT]).type(torch.float32).to("cuda:0")
+    device_1_tensor = torch.from_numpy(
+        host_tensor[NUM_ELEMENT:]).type(torch.float32).to("cuda:1")
+
+    print(
+        f"device_0_tensor device {device_0_tensor.device}\ndevice_1_tensor device {device_1_tensor.device}")
     shard_tensor = qv.ShardTensor(0)
     shard_tensor.append(device_0_tensor)
     shard_tensor.append(device_1_tensor)
     print("shard_tensor shape = ", shard_tensor.shape())
-    
+
     indices = torch.from_numpy(host_indice).type(torch.long)
     indices = indices.to("cuda:0")
-    
+
     # warm up
     feature = shard_tensor[indices]
     torch.cuda.synchronize()
-    
+
     start = time.time()
     feature = shard_tensor[indices]
     torch.cuda.synchronize()
     consumed_time = time.time() - start
-    print(f"gathered data shape = {feature.shape}, consumed {time.time() - start}")
+    print(
+        f"gathered data shape = {feature.shape}, consumed {time.time() - start}")
     feature = feature.cpu().numpy()
     feature_gt = host_tensor[host_indice]
     assert np.array_equal(feature, feature_gt), "TEST FAILED"
-    print(f"TEST SUCCEED!, With Memory Bandwidth = {feature.size * 4 / consumed_time / 1024 / 1024 / 1024} GB/s")
+    print(
+        f"TEST SUCCEED!, With Memory Bandwidth = {feature.size * 4 / consumed_time / 1024 / 1024 / 1024} GB/s")
+
 
 test_shard_tensor_intra_process()
-    
-    

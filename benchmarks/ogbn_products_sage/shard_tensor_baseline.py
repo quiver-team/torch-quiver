592d72c2c64c51d9e024a5a4852e5cb9b7416280
eedalong <ee_dalong@163.com>
2021-08-30 16:08:33 +0800

    add benchmark

--- benchmarks/ogbn_products_sage/shard_tensor_baseline.py
+++ benchmarks/ogbn_products_sage/shard_tensor_baseline.py
@@ -177,7 +177,6 @@ def train(epoch):
     for seeds in train_loader:
         start_time = time.time()
         n_id, batch_size, adjs = sample(seeds, [15, 10, 5])
-        
         print(f"sample consumed = {time.time() - start_time}")
         optimizer.zero_grad()
         nid = n_id.to("cuda")
@@ -198,6 +197,31 @@ def train(epoch):
 
     return loss, approx_acc
 
+
+
+@torch.no_grad()
+def test():
+    model.eval()
+
+    out = model.inference(x)
+
+    y_true = y.cpu().unsqueeze(-1)
+    y_pred = out.argmax(dim=-1, keepdim=True)
+
+    train_acc = evaluator.eval({
+        'y_true': y_true[split_idx['train']],
+        'y_pred': y_pred[split_idx['train']],
+    })['acc']
+    val_acc = evaluator.eval({
+        'y_true': y_true[split_idx['valid']],
+        'y_pred': y_pred[split_idx['valid']],
+    })['acc']
+    test_acc = evaluator.eval({
+        'y_true': y_true[split_idx['test']],
+        'y_pred': y_pred[split_idx['test']],
+    })['acc']
+
+    return train_acc, val_acc, test_acc
 
 
 test_accs = []

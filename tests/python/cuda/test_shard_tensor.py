e4da6463e17ab447298edc190a585ca56d15fff2
eedalong <ee_dalong@163.com>
2021-09-05 11:25:29 +0800

    add

--- tests/python/cuda/test_shard_tensor.py
+++ tests/python/cuda/test_shard_tensor.py
@@ -67,6 +67,23 @@ def test_shard_tensor_intra_process():
     print(
         f"TEST SUCCEED!, With Memory Bandwidth = {feature.size * 4 / consumed_time / 1024 / 1024 / 1024} GB/s")
 
+def child_proc(mem_handle0, device_0, shape0, mem_handle1, device_1, shape1):
+    item0 = qv.ShardTensorItem()
+    item0.mem_handle = mem_handle0
+    item0.device = device_0
+    item0.shape = shape0
+    
+    item1 = qv.ShardTensorItem()
+    item1.mem_handle = mem_handle1
+    item1.device = device_1
+    item1.shape = shape1
+    
+    shard_tensor = qv.ShardTensor(1)
+    shard_tensor.append(item0)
+    shard_tensor.append(item1)
+    
+    
+    
 
 def test_shard_tensor_ipc():
     NUM_ELEMENT = 1000000
@@ -95,40 +112,10 @@ def test_shard_tensor_ipc():
     shard_tensor2.append(device_1_tensor, 1)
     
     ipc_res = shard_tensor2.share_ipc()
-    print(ipc_res)
-    
-    ###################################
-    # Create Shard Tensor With IPC
-    #################################
-    shard_tensor = qv.ShardTensor(0)
-    for item in ipc_res:
-        shard_tensor2.append(item)
-    
-     # warm up
-    feature = shard_tensor[indices]
-    torch.cuda.synchronize()
-
-    start = time.time()
-    feature = shard_tensor[indices]
-    torch.cuda.synchronize()
-    consumed_time = time.time() - start
-    print(
-        f"gathered data shape = {feature.shape}, consumed {time.time() - start}")
-    torch.cuda.synchronize()
-    whole_tensor = torch.from_numpy(
-        host_tensor).type(torch.float32).to("cuda:0")
-    start = time.time()
-    res = whole_tensor[indices]
-    torch.cuda.synchronize()
-    print(
-        f"gathered data shape using torch tensor = {res.shape}, consumed {time.time() - start}")
-
-    feature = feature.cpu().numpy()
-    feature_gt = host_tensor[host_indice]
-    assert np.array_equal(feature, feature_gt), "TEST FAILED"
-    print(
-        f"TEST SUCCEED!, With Memory Bandwidth = {feature.size * 4 / consumed_time / 1024 / 1024 / 1024} GB/s")
-    
+    process = mp.Process(target=child_proc, args = (ipc_res[0].mem_handle, ipc_res[0].device, ipc_res[0].shape,
+                                                    ipc_res[1].mem_handle, ipc_res[1].device, ipc_res[1].shape)
+                        )
+    process.join()
     gc.enable()
     
     

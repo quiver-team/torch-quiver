436ecbdc549d2b14b1f71fabc86310e6961267e0
eedalong <ee_dalong@163.com>
2021-09-15 12:57:47 +0000

    add multi device example

--- benchmarks/ogbn_products_sage/pyg_zero_copy_shard_tensor.py
+++ benchmarks/ogbn_products_sage/pyg_zero_copy_shard_tensor.py
@@ -1,4 +1,5 @@
 import os
+import os.path as osp
 
 import torch
 from tqdm import tqdm
@@ -6,11 +7,40 @@ import torch.nn.functional as F
 import torch.multiprocessing as mp
 import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel
+import numpy as np
+from scipy.sparse import csr_matrix
+from quiver.async_cuda_sampler import AsyncCudaNeighborSampler
 
 from torch_geometric.nn import SAGEConv
 from torch_geometric.datasets import Reddit
 from torch_geometric.data import NeighborSampler
 import time
+from quiver.shard_tensor import ShardTensor as PyShardTensor
+from quiver.shard_tensor import ShardTensorConfig
+
+from ogb.nodeproppred import PygNodePropPredDataset, Evaluator
+
+from typing import List, NamedTuple, Optional, Tuple
+
+
+class InterProcData:
+    edge_index = None
+    train_idx = None
+    val_idx = None
+    test_idx = None
+    y = None
+    num_features = None
+    num_classes = None
+
+
+class Adj(NamedTuple):
+    edge_index: torch.Tensor
+    e_id: torch.Tensor
+    size: Tuple[int, int]
+
+    def to(self, *args, **kwargs):
+        return Adj(self.edge_index.to(*args, **kwargs),
+                   self.e_id.to(*args, **kwargs), self.size)
 
 class SAGE(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels,
@@ -57,38 +87,74 @@ class SAGE(torch.nn.Module):
 
         return x_all
 
+def get_csr_from_coo(edge_index):
+    src = edge_index[0].numpy()
+    dst = edge_index[1].numpy()
+    node_count = max(np.max(src), np.max(dst))
+    data = np.zeros(dst.shape, dtype=np.int32)
+    csr_mat = csr_matrix(
+        (data, (edge_index[0].numpy(), edge_index[1].numpy())))
+    return csr_mat
 
-def run(rank, world_size, dataset):
+def sample(sampler, device, input_nodes, sizes):
+    nodes = input_nodes.to(device)
+    adjs = []
+
+    batch_size = len(nodes)
+    for size in sizes:
+        out, cnt = sampler.sample_layer(nodes, size)
+        frontier, row_idx, col_idx = sampler.reindex(nodes, out, cnt)
+        row_idx, col_idx = col_idx, row_idx
+        edge_index = torch.stack([row_idx, col_idx], dim=0)
+
+        adj_size = torch.LongTensor([
+            frontier.size(0),
+            nodes.size(0),
+        ])
+        e_id = torch.tensor([])
+        adjs.append(Adj(edge_index, e_id, adj_size))
+        nodes = frontier
+
+    return nodes, batch_size, adjs[::-1]
+def run(rank, world_size, shard_tensor_ipc_handle, inter_proc_data: InterProcData):
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12355'
-    dist.init_process_group('nccl', rank=rank, world_size=world_size)
+    #torch.cuda.set_device(rank)
+    #dist.init_process_group('nccl', rank=rank, world_size=world_size)
 
-    data = dataset[0]
-    train_idx = data.train_mask.nonzero(as_tuple=False).view(-1)
-    train_idx = train_idx.split(train_idx.size(0) // world_size)[rank]
+    train_idx = inter_proc_data.train_idx
 
-    train_loader = NeighborSampler(data.edge_index, node_idx=train_idx,
+    csr_mat = get_csr_from_coo(inter_proc_data.edge_index)
+    sampler = AsyncCudaNeighborSampler(csr_indptr=csr_mat.indptr, csr_indices=csr_mat.indices, device=rank, copy=False)
+
+    #train_loader = torch.utils.data.DataLoader(train_idx, batch_size=4096, shuffle=True, drop_last=True)
+    train_loader = NeighborSampler(inter_proc_data.edge_index, node_idx=train_idx,
                                    sizes=[15, 10, 5], batch_size=4096,
-                                   shuffle=True, num_workers=12)
+                                   shuffle=True, num_workers=1)
 
     if rank == 0:
-        subgraph_loader = NeighborSampler(data.edge_index, node_idx=None,
+        subgraph_loader = NeighborSampler(inter_proc_data.edge_index, node_idx=None,
                                           sizes=[-1], batch_size=2048,
                                           shuffle=False, num_workers=6)
 
     torch.manual_seed(12345)
-    model = SAGE(dataset.num_features, 256, dataset.num_classes, num_layers=3).to(rank)
-    model = DistributedDataParallel(model, device_ids=[rank])
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    model = SAGE(inter_proc_data.num_features, 256, inter_proc_data.num_classes, num_layers=3).to(rank)
+    #model = DistributedDataParallel(model, device_ids=[rank])
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.003)
 
-    x, y = data.x.to(rank), data.y.to(rank)
+    y = inter_proc_data.y.squeeze().to(rank)
+    x = PyShardTensor.new_from_share_ipc(shard_tensor_ipc_handle)
 
     for epoch in range(1, 21):
         model.train()
         start_time = time.time()
+        #for seeds in train_loader:
+        #    n_id, batch_size, adjs = sample(sampler, rank, seeds, [15, 10, 5])
         for batch_size, n_id, adjs in train_loader:
             if rank == 0:
                 print(f"data time = {time.time()  - start_time}")
+            feature = x[n_id]
+            '''
             adjs = [adj.to(rank) for adj in adjs]
 
             optimizer.zero_grad()
@@ -98,31 +164,46 @@ def run(rank, world_size, dataset):
             optimizer.step()
             if rank == 0:
                 print(f"iter time = {time.time()  - start_time}")
+            '''
             start_time = time.time()
 
-        dist.barrier()
+    #    dist.barrier()
 
-        if rank == 0:
-            print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}')
+        #if rank == 0:
+        #    print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}')
 
-        if rank == 0 and epoch % 5 == 0:  # We evaluate on a single GPU for now
-            model.eval()
-            with torch.no_grad():
-                out = model.module.inference(x, rank, subgraph_loader)
-            res = out.argmax(dim=-1) == data.y
-            acc1 = int(res[data.train_mask].sum()) / int(data.train_mask.sum())
-            acc2 = int(res[data.val_mask].sum()) / int(data.val_mask.sum())
-            acc3 = int(res[data.test_mask].sum()) / int(data.test_mask.sum())
-            print(f'Train: {acc1:.4f}, Val: {acc2:.4f}, Test: {acc3:.4f}')
-
-        dist.barrier()
+    #    dist.barrier()
 
     dist.destroy_process_group()
 
 
 if __name__ == '__main__':
-    dataset = Reddit('/home/dalong/Reddit')
+    torch.cuda.set_device(0)
+    home = os.getenv('HOME')
+    data_dir = osp.join(home, '.pyg')
+    root = osp.join(data_dir, 'data', 'products')
+    dataset = PygNodePropPredDataset('ogbn-products', root)
+    
+    split_idx = dataset.get_idx_split()
+    train_idx = split_idx['train']
 
-    world_size = torch.cuda.device_count()
+    data = dataset[0]
+    inter_proc_data = InterProcData()
+    data.edge_index.share_memory_()
+
+    inter_proc_data.edge_index = data.edge_index
+    inter_proc_data.num_classes = dataset.num_classes
+    inter_proc_data.num_features = dataset.num_features
+    inter_proc_data.train_idx = train_idx
+    
+    inter_proc_data.y = data.y
+
+    shard_tensor_config = ShardTensorConfig({})
+    shard_tensor = PyShardTensor(0, shard_tensor_config)
+    shard_tensor.from_cpu_tensor(data.x)
+    ipc_handle = shard_tensor.share_ipc()
+
+    world_size = 1 #torch.cuda.device_count()
     print('Let\'s use', world_size, 'GPUs!')
-    mp.spawn(run, args=(world_size, dataset), nprocs=world_size, join=True)
+    mp.spawn(run, args=(world_size, ipc_handle, inter_proc_data), nprocs=world_size, join=True)
+    #run(0, world_size, shard_tensor, inter_proc_data)

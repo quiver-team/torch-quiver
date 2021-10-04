d797b22d51d4f75fd8edf7dcc36f109e654f097e
eedalong <ee_dalong@163.com>
2021-10-04 14:54:59 +0000

    add graph sage sampler

--- srcs/python/quiver/sage_sampler.py
+++ srcs/python/quiver/sage_sampler.py
@@ -5,18 +5,38 @@ import os
 import time
 import numpy as np
 
-
+from torch_sparse import SparseTensor
 import torch
 import torch_quiver as qv
 
 
 class GraphSageSampler:
-    def __init__(self, edge_index, sizes, device, mode="UVA", device_replicate=True):
+
+    def __init__(self, edge_index: Union[Tensor, SparseTensor], sizes: List[int], device, num_nodes: Optional[int] = None, mode="UVA", device_replicate=True):
+        edge_index = edge_index.to("cpu")
+        self.is_sparse_tensor = isinstance(edge_index, SparseTensor)
+
         self.sizes = sizes
         
         self.quiver = None
-        self.edge_index = None
+           # Obtain a *transposed* `SparseTensor` instance.
+        if not self.is_sparse_tensor:
+            if num_nodes is None:
+                num_nodes = int(edge_index.max()) + 1
+
+            value = None
+            self.adj_t = SparseTensor(row=edge_index[0], col=edge_index[1], value=value, sparse_sizes=(num_nodes, num_nodes)).t()
+        else:
+            self.adj_t = edge_index
+
         self.mode = mode
+        if self.mode == "UVA":
+            indptr, indices, _ = self.adj_t.csr()
+            edge_id = torch.zeros(1, dtype=torch.long)
+            self.quiver = qv.new_quiver_from_csr_array(indptr, indices, edge_id, device, device_replicate)
+        else:
+            pass
+        
         self.device_replicate = device_replicate
         self.device = device
 

319d759ac81ad5f63e4570f741843504dffaa010
lg <lg4869@outlook.com>
2021-10-06 15:41:57 +0000

    cleanup cmake

--- srcs/cmake/torch.cmake
+++ srcs/cmake/torch.cmake
@@ -1,18 +1,13 @@
 INCLUDE(srcs/cmake/generated/torch.cmake)
 
-FUNCTION(TARGET_SOURCES_GLOB target)
-    FILE(GLOB SRCS ${ARGN})
-    TARGET_SOURCES(${target} PRIVATE ${SRCS})
-ENDFUNCTION()
-
 # ADD_LIBRARY(torch_quiver SHARED)
 ADD_LIBRARY(torch_quiver STATIC)
 TARGET_INCLUDE_DIRECTORIES(torch_quiver
                            PUBLIC ${CMAKE_SOURCE_DIR}/srcs/cpp/include)
 
-TARGET_SOURCES_GLOB(torch_quiver ${CMAKE_SOURCE_DIR}/srcs/cpp/src/quiver/*.cpp)
-TARGET_SOURCES_GLOB(torch_quiver
-                    ${CMAKE_SOURCE_DIR}/srcs/cpp/src/quiver/cpu/*.cpp)
+TARGET_SOURCE_DIR(torch_quiver ${CMAKE_SOURCE_DIR}/srcs/cpp/src/quiver/*.cpp)
+TARGET_SOURCE_DIR(torch_quiver
+                  ${CMAKE_SOURCE_DIR}/srcs/cpp/src/quiver/cpu/*.cpp)
 
 IF(ENABLE_CUDA)
     ENABLE_LANGUAGE(CUDA)
@@ -20,8 +15,8 @@ IF(ENABLE_CUDA)
     LINK_DIRECTORIES(${CUDA_TOOLKIT_ROOT_DIR}/lib64)
     TARGET_COMPILE_OPTIONS(
         torch_quiver PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:--expt-extended-lambda>)
-    TARGET_SOURCES_GLOB(torch_quiver
-                        ${CMAKE_SOURCE_DIR}/srcs/cpp/src/quiver/cuda/*.cpp)
-    TARGET_SOURCES_GLOB(torch_quiver
-                        ${CMAKE_SOURCE_DIR}/srcs/cpp/src/quiver/cuda/*.cu)
+    TARGET_SOURCE_DIR(torch_quiver
+                      ${CMAKE_SOURCE_DIR}/srcs/cpp/src/quiver/cuda/*.cpp)
+    TARGET_SOURCE_DIR(torch_quiver
+                      ${CMAKE_SOURCE_DIR}/srcs/cpp/src/quiver/cuda/*.cu)
 ENDIF()

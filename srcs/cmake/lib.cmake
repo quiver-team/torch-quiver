5b250dc2fbfd32943f3566e1a8feb62b1f335128
lg <lg4869@outlook.com>
2021-10-06 13:03:24 +0000

    cleanup cmake

--- srcs/cmake/lib.cmake
+++ srcs/cmake/lib.cmake
@@ -1,8 +1,15 @@
-ADD_LIBRARY(quiver SHARED)
-TARGET_ADD_SOURCE_PATTERN(quiver srcs/cpp/src//hybrid/*.cpp)
+FUNCTION(BUILD_QUIVER_LIB target)
+    ADD_LIBRARY(${target} SHARED)
+    TARGET_SOURCE_TREE(${target} ${CMAKE_SOURCE_DIR}/srcs/cpp/src/*.cpp)
 
-IF(ENABLE_CUDA)
-    TARGET_ADD_SOURCE_PATTERN(quiver srcs/cpp/src/hybrid/sample/*.cu)
-    TARGET_INCLUDE_DIRECTORIES(quiver PRIVATE ${CUDA_INCLUDE_DIRS})
-    TARGET_LINK_LIBRARIES(quiver ${CUDA_LIBRARIES})
-ENDIF()
+    IF(ENABLE_CUDA)
+        TARGET_SOURCE_TREE(${target} ${CMAKE_SOURCE_DIR}/srcs/cpp/src/*.cu)
+        TARGET_INCLUDE_DIRECTORIES(${target} PRIVATE ${CUDA_INCLUDE_DIRS})
+        TARGET_LINK_LIBRARIES(${target} ${CUDA_LIBRARIES})
+        TARGET_COMPILE_OPTIONS(
+            ${target}
+            PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:--expt-extended-lambda>)
+    ENDIF()
+ENDFUNCTION()
+
+BUILD_QUIVER_LIB(quiver)

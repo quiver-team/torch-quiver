375afb9db3620795e53f0877689e9c72b13d51e4
eedalong <ee_dalong@163.com>
2021-10-17 05:31:47 +0000

    fix samper benchmark

--- benchmarks/sample/bench_sampler.py
+++ benchmarks/sample/bench_sampler.py
@@ -12,34 +12,42 @@ print("\n\nNOTE: We Use Sampled Edges Per Second(SEPS) = #SampledEdges/Time as m
 
 def bench_on_ogbproduct():
     print("=" * 20  + "OGBn-Product Gpex" + "=" * 20)
-    root = "/home/dalong/data/products"
+    root = "/home/dalong/products"
     dataset = PygNodePropPredDataset('ogbn-products', root)
     train_idx = dataset.get_idx_split()["train"]
     train_loader = torch.utils.data.DataLoader(train_idx, batch_size=1024, pin_memory=True, shuffle=True)
     csr_topo = quiver.CSRTopo(dataset[0].edge_index)
     quiver_sampler = quiver.pyg.GraphSageSampler(csr_topo, [15, 10, 5], device=0, mode="UVA")
 
-    sample_start = time.time()
+    sample_time = 0
     sampled_edges = 0
     for seeds in train_loader:
+        seeds = seeds.to(0)
+        torch.cuda.synchronize()
+        sample_start = time.time()
         _, _, adjs = quiver_sampler.sample(seeds)
+        torch.cuda.synchronize()
+        sample_time += time.time() - sample_start
         for adj in adjs:
             sampled_edges += adj.edge_index.shape[1]
-    print(f"mean degree {np.mean(csr_topo.degree.numpy())}\tSample Speed {sampled_edges / (time.time() - sample_start) / 1000000}M SEPS")
+    print(f"mean degree {np.mean(csr_topo.degree.numpy())}\tSample Speed {sampled_edges / sample_time / 1000000}M SEPS")
 
 def bench_on_ogbproduct_cpu():
     print("=" * 20  + "OGBn-Product CPU" + "=" * 20)
-    root = "/home/dalong/data/products"
+    root = "/home/dalong/products"
     dataset = PygNodePropPredDataset('ogbn-products', root)
     train_idx = dataset.get_idx_split()["train"]
     train_loader = NeighborSampler(dataset[0].edge_index, node_idx=train_idx, sizes=[15, 10, 5], batch_size=1024, shuffle=True)
 
-    sample_start = time.time()
+    sample_time = 0
     sampled_edges = 0
+    sample_start = time.time()
     for batch_size, n_id, adjs in train_loader:
+        sample_time += time.time() - sample_start
         for adj in adjs:
             sampled_edges += adj.edge_index.shape[1]
-    print(f"Sample Speed {sampled_edges / (time.time() - sample_start) / 1000000}M SEPS")
+        sample_start = time.time()
+    print(f"Sample Speed {sampled_edges / sample_time / 1000000}M SEPS")
 
 
 def bench_on_reddit():
@@ -51,27 +59,35 @@ def bench_on_reddit():
     csr_topo = quiver.CSRTopo(dataset[0].edge_index)
     quiver_sampler = quiver.pyg.GraphSageSampler(csr_topo, [25, 10], device=0, mode="UVA")
 
-    sample_start = time.time()
+    sample_time = 0
     sampled_edges = 0
     for seeds in train_loader:
+        seeds = seeds.to(0)
+        torch.cuda.synchronize()
+        sample_start = time.time()
         _, _, adjs = quiver_sampler.sample(seeds)
+        torch.cuda.synchronize()
+        sample_time += time.time() - sample_start
         for adj in adjs:
             sampled_edges += adj.edge_index.shape[1]
-    print(f"mean degree {np.mean(csr_topo.degree.numpy())}\tSample Speed {sampled_edges / (time.time() - sample_start) / 1000000}M SEPS")
+    print(f"mean degree {np.mean(csr_topo.degree.numpy())}\tSample Speed {sampled_edges / sample_time / 1000000}M SEPS")
 
 def bench_on_reddit_cpu():
     print("=" * 20  + "Reddit CPU" + "=" * 20)
     dataset = Reddit('/home/dalong/data/Reddit')
     train_mask = dataset[0].train_mask
     train_idx = train_mask.nonzero(as_tuple=False).view(-1)
-    train_loader = NeighborSampler(dataset[0].edge_index, node_idx=train_idx, sizes=[15, 10, 5], batch_size=1024, shuffle=True)
+    train_loader = NeighborSampler(dataset[0].edge_index, node_idx=train_idx, sizes=[25, 10], batch_size=1024, shuffle=True)
 
     sample_start = time.time()
+    sample_time = 0
     sampled_edges = 0
     for batch_size, n_id, adjs in train_loader:
+        sample_time += time.time() - sample_start
         for adj in adjs:
             sampled_edges += adj.edge_index.shape[1]
-    print(f"Sample Speed {sampled_edges / (time.time() - sample_start) / 1000000}M SEPS")
+        sample_start = time.time()
+    print(f"Sample Speed {sampled_edges / sample_time / 1000000}M SEPS")
 
 
 def bench_on_paper100M():
@@ -91,13 +107,16 @@ def bench_on_paper100M():
     csr_topo = quiver.CSRTopo(indptr=indptr, indices=indices)
     quiver_sampler = quiver.pyg.GraphSageSampler(csr_topo, [15, 10, 5], device=0, mode="UVA")
 
-    sample_start = time.time()
+    
     sampled_edges = 0
+    sample_time = 0
     for seeds in train_loader:
+        sample_start = time.time()
         _, _, adjs = quiver_sampler.sample(seeds)
+        sample_time += time.time() - sample_start
         for adj in adjs:
             sampled_edges += adj.edge_index.shape[1]
-    print(f"mean degree {np.mean(csr_topo.degree.numpy())}\tSample Speed {sampled_edges / (time.time() - sample_start) / 1000000}M SEPS")
+    print(f"mean degree {np.mean(csr_topo.degree.numpy())}\tSample Speed {sampled_edges / sample_time / 1000000}M SEPS")
 
 if __name__ == "__main__":
     #bench_on_ogbproduct()

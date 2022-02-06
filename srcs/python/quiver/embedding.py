4df0d2b20504898b0dd395e934ea65ebf776d4c8
Yao Fu <fuyao3860@gmail.com>
2022-02-06 15:37:26 +0000

    feat: add embedding (draft)

---
+++ srcs/python/quiver/embedding.py
@@ -0,0 +1,34 @@
+import torch
+from torch import nn
+
+from quiver.shard_tensor import ShardTensor, ShardTensorConfig, Topo
+from quiver.utils import reindex_feature, CSRTopo
+from typing import List
+import numpy as np
+from torch._C import device
+
+
+class Embedding(nn.Module):
+    def __init__(self, n_embeddings: int, d_embeddings: int, rank: int,
+                 device_list: List[int]):
+        super().__init__()
+        self.rank = rank
+        self.device_list = device_list
+        self.topo = Topo(self.device_list)
+
+        self.shard_tensor = ShardTensor(self.rank, ShardTensorConfig({}))
+
+        n_shards = len(device_list)+1
+        items_per_shard = (n_embeddings+n_shards-1)//n_shards
+        for idx, device in self.device_list:
+            embedding_weight = torch.randn(items_per_shard, d_embeddings)
+            self.shard_tensor.append(embedding_weight, device)
+            del embedding_weight
+
+        items_remained = n_embeddings-(n_shards-1)*items_per_shard
+        embedding_weight = torch.randn(items_remained, d_embeddings)
+        self.shard_tensor.append(embedding_weight, -1)
+        del embedding_weight
+
+    def forward(self, index):
+        return self.shard_tensor[index]

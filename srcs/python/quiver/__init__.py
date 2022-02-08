3ac20cac51a26c4f619607b1e86ebff77bf4899a
Yao Fu <fuyao3860@gmail.com>
2022-02-08 09:47:39 +0000

    feat: multiprocessing embedding

--- srcs/python/quiver/__init__.py
+++ srcs/python/quiver/__init__.py
@@ -1,12 +1,12 @@
 
 from .feature import Feature, DistFeature, PartitionInfo
+from .embedding import Embedding
 from .pyg import GraphSageSampler, MixedGraphSageSampler, SampleJob
 from . import multiprocessing
 from .utils import CSRTopo
 from .utils import Topo as p2pCliqueTopo
 from .utils import init_p2p
 from .comm import NcclComm, getNcclId
-from .embedding import Embedding
 
 __all__ = [
     "Feature", "DistFeature", "GraphSageSampler", "PartitionInfo", "CSRTopo",
